78c94abcbb04937a9a84bd02b9ba56e8639a7f7d
carolyyyy <47976237+carolyyyy@users.noreply.github.com>
2020-09-12 22:28:09 +0800

    add view function

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'demo-accordion-opened',
   templateUrl: 'home.component.html'
 })
 export class HomeComponent {
 
   constructor() { }
 
+  isCollapsed: boolean = false;
+  iconCollapse: string = 'icon-arrow-up';
+//   months = [1,2,3,4,5,6,7,8,9,10,11,12];
+   month = new Date().getMonth()+1;
+   year = new Date().getFullYear();
+
+  catogories = [
+    {
+      name: 'Utilities',
+      money: 1010.23,
+      records: [
+        {
+          name: 'book',
+          money: 10.23,
+          date: '2020/09/12'
+        },
+        {
+          name: 'printer',
+          money: 1000.00,
+          date: '2020/09/12'
+        }
+      ]
+    },
+    {
+      name: 'travel',
+      money: 2000.00,
+      records: [
+        {
+          name: 'traffic',
+          money: 1000.00,
+          date: '2020/09/12'
+        },
+        {
+          name: 'hotel',
+          money: 1000.00,
+          date: '2020/09/12'
+        }
+      ]
+    }
+  ]
+
+  collapsed(event: any): void {
+    // console.log(event);
+  }
+
+  expanded(event: any): void {
+    // console.log(event);
+  }
+
+  toggleCollapse(): void {
+    this.isCollapsed = !this.isCollapsed;
+    this.iconCollapse = this.isCollapsed ? 'icon-arrow-down' : 'icon-arrow-up';
+  }
+
 }
+
+
+
+
--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -1,19 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   imports: [
+    CommonModule,
     FormsModule,
     HomeRoutingModule,
     ChartsModule,
     BsDropdownModule,
-    ButtonsModule.forRoot()
+    ButtonsModule.forRoot(),
+
+    AccordionModule.forRoot()
   ],
   declarations: [ HomeComponent ]
 })
