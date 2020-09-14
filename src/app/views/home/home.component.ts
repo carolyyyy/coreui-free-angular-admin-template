859bcb2f61ebf8a0783bd3d5119b9ab5462eed1c
carolyyyy <47976237+carolyyyy@users.noreply.github.com>
2020-09-14 16:19:11 +0800

    version for no data connected

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -17,11 +17,16 @@ export class HomeComponent {
   isCollapsed: boolean = false;
   iconCollapse: string = 'icon-arrow-up';
 //   months = [1,2,3,4,5,6,7,8,9,10,11,12];
-   month = new Date().getMonth()+1;
-   year = new Date().getFullYear();
+  //  month = new Date().getMonth()+1;
+  //  year = new Date().getFullYear();
+
+   secondDate: Date = new Date();
+   firstDate: Date = new Date(this.secondDate.getFullYear(),this.secondDate.getMonth(),1);
+   bsRangeValue: Date[] = [this.firstDate, this.secondDate];
 
    addNote: string;
-   addMoney: number;
+   addMoney: string;
+   addOutcome: string;
    addDate: Date = new Date();
    addCatogory: string;
 
@@ -31,7 +36,8 @@ export class HomeComponent {
    deleteCatogoryName: string;
 
    editRecordNote: string;
-   editRecordMoney: number;
+   editRecordMoney: string;
+   editRecordOutcome: string;
    editRecordDate: Date;
    editRecordCatogory: string;
 
@@ -43,11 +49,13 @@ export class HomeComponent {
         {
           note: 'book',
           money: -10.23,
+          isOutcome: true,
           date: new Date('2020/9/12')
         },
         {
           note: 'printer',
           money: -1000.00,
+          isOutcome: true,
           date: new Date('2020/9/12')
         }
       ]
@@ -59,16 +67,30 @@ export class HomeComponent {
         {
           note: 'traffic',
           money: -1000.00,
+          isOutcome: true,
           date: new Date('2020/9/12')
         },
         {
           note: 'hotel',
           money: -1000.00,
+          isOutcome: true,
           date: new Date('2020/9/12')
         }
       ]
     }
   ]
+
+  categories = [{
+    id: 1,
+    name: 'Utilities'
+  },{
+    id: 2,
+    name: 'Travel'
+  }]
+
+  change(){
+    console.log(this.addOutcome);
+  }
 
   collapsed(event: any): void {
     // console.log(event);
@@ -87,10 +109,16 @@ export class HomeComponent {
     this.modalRef = this.modalService.show(template);
   }
 
-  openModalWithEditRecord(template: TemplateRef<any>, recordNote: string, recordMoney: number, recordDate: Date, recordCatogory: string) {
+  openModalWithEditRecord(template: TemplateRef<any>, recordNote: string, recordMoney: string, recordOutcome: boolean, recordDate: Date, recordCatogory: string) {
     this.modalRef = this.modalService.show(template);
     console.log(recordNote);
     console.log(recordMoney);
+    if(recordOutcome){
+      this.editRecordOutcome = 'Outcome';
+    }else{
+      this.editRecordOutcome = 'Income';
+    }
+    
     this.editRecordNote = recordNote;
     this.editRecordMoney = recordMoney;
     this.editRecordDate = recordDate;
@@ -113,9 +141,15 @@ export class HomeComponent {
     this.modalRef = this.modalService.show(template);
   }
 
-  confirm(): void {
+  confirm(recordNote: string, recordMoney: string, recordOutcome: boolean, recordDate: Date, recordCatogory: string): void {
     // this.message = 'Confirmed!';
+    //call delRecord getAllRecords
     this.modalRef.hide();
+    for(var i=0;i<this.catogories.length;i++){
+      if(this.catogories[i].name == recordCatogory){
+
+      }
+    }
   }
  
   decline(): void {
@@ -123,27 +157,167 @@ export class HomeComponent {
     this.modalRef.hide();
   }
 
+  addRecordClose(){
+    this.modalRef.hide();
+    this.addNote = null;
+    this.addMoney = null;
+    this.addOutcome = null;
+    this.addDate = new Date();
+    this.addCatogory = null;
+  }
+
   addRecord(){
     if(this.addNote == null){
       alert("Note can not be empty!");
     }else if(this.addMoney == null){
       alert("Money can not be empty!");
-    }else if(/^-?\d+\.?\d+$/.test(this.addMoney.toString())){
+    }else if(isNaN(parseFloat(this.addMoney))){
       alert("Money must be a valid number!");
+    }else if(this.addOutcome == null){
+      alert("Record must have a type of outcome or income!");
     }else if(this.addCatogory == null){
       alert("Catogory can not be empty!");
     }else{
+      //call addRecord getAllRecords
       for(var i=0;i<this.catogories.length;i++){
         if(this.catogories[i].name == this.addCatogory){
           this.catogories[i].records.push({
             note: this.addNote,
-            money: this.addMoney,
+            isOutcome: Boolean(this.addOutcome=='Outcome'),
+            money: parseFloat(this.addMoney.substring(0,this.addMoney.indexOf(".")+3)),
             date: this.addDate})
+            this.modalRef.hide();
+            this.addNote = null;
+            this.addMoney = null;
+            this.addOutcome = null;
+            this.addDate = new Date();
+            this.addCatogory = null;
             break;
         }
       }
     }
+    
+  }
+
+  addCatogoryFunctionClose(){
     this.modalRef.hide();
+    this.addCatogoryName = null;
+  }
+
+  addCatogoryFunction(){
+    if(this.addCatogoryName == null){
+      alert("Category name can not be empty!");
+    }else{
+      for(var i=0;i<this.categories.length;i++){
+        if(this.addCatogoryName == this.categories[i].name){
+          alert("Category name can not be repeated!");
+          return;
+        }
+      }
+      //call addCategory getAllCats
+      this.categories.push({
+        id: this.categories[this.categories.length-1].id+1,
+        name: this.addCatogoryName
+      })
+      this.modalRef.hide();
+      this.addCatogoryName = null;
+    }
+
+  }
+
+  editCategoryFunctionClose(){
+    this.modalRef.hide();
+    this.editCatogorySelectName = null;
+    this.editCatogoryName = null;
+  }
+
+  editCategoryFunction(){
+    if(this.editCatogorySelectName == null){
+      alert("Please select a category which is going to be edited!");
+    }else if(this.editCatogoryName == null){
+      alert("Category name can not be empty!");
+    }else{
+
+      for(var i=0;i<this.categories.length;i++){
+        if(this.editCatogoryName == this.categories[i].name){
+          alert("Category name can not be repeated!");
+          return;
+        }
+      }
+      //call editCat
+
+        for(var i=0;i<this.categories.length;i++){
+       
+          if(this.editCatogorySelectName == this.categories[i].name){
+            this.categories[i].name = this.editCatogoryName;
+            break;
+          }
+        }
+  
+        for(var i=0;i<this.catogories.length;i++){
+          if(this.editCatogorySelectName == this.catogories[i].name){
+            this.catogories[i].name = this.editCatogoryName;
+            break;
+          }
+  
+        this.modalRef.hide();
+        this.editCatogorySelectName = null;
+        this.editCatogoryName = null;
+      }
+
+    }
+  }
+
+  deleteCategoryFunctionClose(){
+    this.modalRef.hide();
+    this.deleteCatogoryName = null;
+  }
+
+  deleteCategoryFunction(){
+    if(this.deleteCatogoryName == null){
+      alert("Category name can not be empty!");
+    }else{
+            //call delCat
+      for(var i=0;i<this.categories.length;i++){
+        if(this.categories[i].name == this.deleteCatogoryName){
+          this.categories.splice(i,1);
+          
+          break;
+        }
+      }
+
+      for(var i=0;i<this.catogories.length;i++){
+        if(this.catogories[i].name == this.deleteCatogoryName){
+          this.catogories.splice(i,1);
+          break;
+        }
+      }
+
+      this.modalRef.hide();
+      this.deleteCatogoryName = null;
+    }
+  }
+
+  editRecordFunctionClose(){
+    this.modalRef.hide();
+
+  }
+
+  editRecordFunction(){
+    if(this.editRecordNote == null){
+      alert("Note can not be empty!");
+    }else if(this.editRecordMoney == null){
+      alert("Money can not be empty!");
+    }else if(isNaN(parseFloat(this.editRecordMoney))){
+      alert("Money must be a valid number!");
+    }else if(this.editRecordOutcome == null){
+      alert("Record must have a type of outcome or income!");
+    }else if(this.editRecordCatogory == null){
+      alert("Catogory can not be empty!");
+    }else{
+      //call two API: editRecord  getAllRecords
+      
+    }
   }
 
 }
